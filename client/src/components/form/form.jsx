63d0e406f076eed4c95e3aa05aec647d96a225df
Daniele <Barzazzi>
2022-07-07 15:12:08 +0200

    submit

--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -1,22 +1,22 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
-const cors = require('cors')
+
 const MyForm= ()=> {
   return (
     <>
-      <Form.Group controlId="formFile" className="mb-3" >
-        <Form.Label>Default file input example</Form.Label>
-        <Form.Control type="file" />
-        <Button variant="primary" type="submit" onClick={async ()=>{
-            const res=await axios.get('http://localhost:3002/asd');
-            alert(JSON.stringify(res));
-        }}>
-            Submit
-        </Button>
-      </Form.Group>
+      <Form action="http://localhost:3002/file" enctype="multipart/form-data" method="post" >
+        <Form.Group controlId="formFile" className="mb-3" >
+          <Form.Label>Default file input example</Form.Label>
+          <Form.Control type="file" name="uploaded_file"/>
+          <Button variant="primary" type="submit" >
+              Submit
+          </Button>
+        </Form.Group>
+      </Form>
+      
     </>
   );
 }
 
 export default MyForm;
+
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3002
 const cors = require('cors')
+const multer  = require('multer')
+const upload = multer({ dest: './uploads' })
 
 app.use(cors());
     app.use(express.json());
@@ -9,6 +11,13 @@ app.use(cors());
 
 app.route('/asd').get((req, res) => {
     res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.route('/file').post(upload.single('uploaded_file'),(req, res) => {
+  //res.json({msg: 'This is CORS-enabled for all origins!'})
+  console.log(req.file, req.body)
+  res.send("tuttappost");
+  
 })
 
 app.listen(port, () => {
