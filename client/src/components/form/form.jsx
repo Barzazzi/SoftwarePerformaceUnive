1bc809c9ff95913e2ccb1174f7d5daf122088d1f
Berseria <34916697+Berseria@users.noreply.github.com>
2022-09-14 10:38:38 +0200

    pewpew

--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -1,22 +1,62 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
-const cors = require('cors')
-const MyForm= ()=> {
-  return (
-    <>
-      <Form.Group controlId="formFile" className="mb-3" >
-        <Form.Label>Default file input example</Form.Label>
-        <Form.Control type="file" />
-        <Button variant="primary" type="submit" onClick={async ()=>{
-            const res=await axios.get('http://localhost:3002/asd');
-            alert(JSON.stringify(res));
-        }}>
-            Submit
-        </Button>
-      </Form.Group>
-    </>
-  );
+import React from "react";
+import axios, { post } from 'axios';
+
+
+class MyForm extends React.Component{
+    constructor(props) {
+        super(props);
+        
+        this.state ={
+          file:null,
+          accept_only: ".cpp,.cc"
+        };
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    async onFormSubmit (e) {
+        e.preventDefault(); // Stop form submit
+        //alert("questo è il file" + this.state.file.name);
+        const url = 'http://localhost:3002/file';
+        const formData = new FormData();
+        const file= this.state.file;
+        formData.append('file',file, file.name);
+        //alert("contenuto di file"+ formData.getAll("file").name);
+        const res = await axios.post(url,formData)
+        .catch(function (error){
+          alert("error: " + JSON.stringify(error));
+        });
+        alert(JSON.stringify(res.data));
+      }
+
+    onChange(e) {
+      // da introdurre il controllo
+        this.setState({file:e.target.files[0]});
+    }
+
+
+    render() {
+        return (
+          <form onSubmit={this.onFormSubmit}>
+            <h1>File Upload</h1>
+            <input type="file" onChange={this.onChange} />
+            <button type="submit">Upload</button>
+          </form>
+       )
+    }
+
 }
+
+
+
+/*
+
+const res = await axios.post('http://localhost:3002', formData, {
+  headers: formData.getHeaders()
+});*/
+
+
+
+
 
 export default MyForm;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,70 @@
+/* ` */
 const express = require('express')
 const app = express()
 const port = 3002
 const cors = require('cors')
+const multer  = require('multer')
+var result = "";
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+//const Queue = require('bull');
+//const { extendLock } = require('bull/lib/scripts')
+//const myFirstQueue = new Bull('my-first-queue');
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) { 
+     cb(null, './uploads');
+  }, 
+  filename: function (req, file, cb) { 
+     cb(null , file.originalname);
+  }
+});
+/*
+const waitingQueue = new Queue('waiting queue',{
+  redis: {
+      host: '127.0.0.1',
+      port: 6379,
+  }
+});
+*/
+const upload = multer({ storage:storage})
+
 
 app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-app.route('/asd').get((req, res) => {
+app.route('/').get((req,res) => {
+    res.send('questa è la homepage');
+})    
+/*app.route('/asd').post((req, res) => {
     res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+})*/
+
+async function execution (operation){
+  try{
+    const {stdout, stderr}= await exec(operation);
+    return {"stdout":stdout, "result":1};
+  }catch (error){
+    return {"erroreType":error.stderr, "result":2};
+  }
+  
+}
+
+app.route('/file').post(upload.single('file'), async (req,res) => {
+  console.log(req.file);
+  var name="file";
+  const operation = await execution(`gcc -lstdc++ -o ./uploads/${name}.exe ./uploads/${req.file.originalname}`);
+  console.log("finito l'exec: " + operation);
+  if(operation["result"] === 1 ){
+    res.send("ok :D ");
+  }else{
+    res.send(`your request: ${operation["erroreType"]}`);
+  }
+  
+}) 
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
